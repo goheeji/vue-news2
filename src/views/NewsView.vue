e7ef033f0e27b35ffffa04f0a92cf87f14dae3f3
goheeji <gmlwl0489@daum.net>
2022-02-03 16:23:53 +0900

    라우터, API, sotre 구현

---
+++ src/views/NewsView.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <div v-for="user in this.$store.state.news" v-bind:key="user.id">{{ user.title }}</div>
+</div>
+  
+</template>
+
+<script>
+export default {
+    created(){
+        this.$store.dispatch('FETCH_NEWS');
+    }
+}
+</script>
+
+<style>
+
+</style>
